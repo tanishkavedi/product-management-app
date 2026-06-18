4eced219f281c067a71ef2e318dcea6027ec9800
tanishkavedi <160759271+tanishkavedi@users.noreply.github.com>
2026-06-19 03:39:25 +0530

    Implement user data access layer

---
+++ backend/src/models/userModel.ts
@@ -0,0 +1,22 @@
+import pool from "../config/db";
+import { User } from "../types";
+
+export const createUser = async (
+  name : string,
+  email : string ,
+  hashedPassword: String
+): Promise<User> => {
+  const result = await pool.query(
+    `INSERT INTO users (name, email, passowrd)
+    VALUES ($1, $2, $3) RETURNING *`,
+    [name, email, hashedPassword]
+  );
+  return result.rows[0];
+};
+
+export const findUserByEmail = async (email:string): Promise<User | null> => {
+  const resutl = await pool.query(
+    [email]
+  );
+  return Result.rows[0] || null;
+};
