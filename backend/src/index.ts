254db3e0974e86baadb93d4b864e564011afd081
tanishkavedi <160759271+tanishkavedi@users.noreply.github.com>
2026-06-19 04:14:01 +0530

    add product routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import "./config/db";
 import authRoutes from "./routes/authRoutes";
+import productRoutes from "./routes/productRoutes";
 
 
 dotenv.config();
@@ -13,6 +14,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);  
+
+
 
 app.get("/", (req: Request ,res: Response) => {
   res.send("Product Management API running");
