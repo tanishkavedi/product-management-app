e32ff76b40557365c5f8e3409533d7b02d08e47f
tanishkavedi <160759271+tanishkavedi@users.noreply.github.com>
2026-06-19 04:31:12 +0530

    debug

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,12 +1,12 @@
-import axios from "axios";
+import axios, { InternalAxiosRequestConfig } from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   const token = localStorage.getItem("token");
-  if (token) {
+  if (token && config.headers) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
